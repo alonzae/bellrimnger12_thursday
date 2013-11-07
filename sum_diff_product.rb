f53338c900dd2827d092645d4670396afdc89ded
alonzae <apruitt@chicagotechacademy.org>
2013-11-07 15:04:20 -0600

    classwoek

---
+++ sum_diff_product.rb
@@ -0,0 +1,37 @@
+
+printf("The number of integers is: \t\t%d\n", count);
+
+printf("The sum of the integers is: \t\t%d\n", sum);
+
+printf("The average of the integers is: \t%d\n", avg);
+
+printf("The smallest integer is: \t\t%d\n", smal);
+
+printf("The largest integer is: \t\t%d\n", larg);
+
+if (less = 1)
+
+  
+
+printf("At least one number was < 20: \tTrue\n");
+
+  
+
+else printf("At least one number was < 20: \tFalse\n");
+
+if (tween = 1)
+
+  
+
+printf("All numbers were (10 <= n <= 90): \tTrue\n");
+
+  
+
+else printf("All numbers were (10 <= n <= 90): \tFalse\n");
+
+ 
+
+
+return 0;
+
+}
