85ee9fc1d71db05f5ed79751a32190bf96baec75
alonzae <apruitt@chicagotechacademy.org>
2013-11-08 15:04:36 -0600

    friday

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,37 +1,10 @@
+def method(hat,bat) 
+puts"Enter two intergers"
+hat = gets.chomp.to_i
+bat = gets.chomp.to_i
 
-printf("The number of integers is: \t\t%d\n", count);
+puts hat +
+puts bat =
 
-printf("The sum of the integers is: \t\t%d\n", sum);
+method(4,1)
 
-printf("The average of the integers is: \t%d\n", avg);
-
-printf("The smallest integer is: \t\t%d\n", smal);
-
-printf("The largest integer is: \t\t%d\n", larg);
-
-if (less = 1)
-
-  
-
-printf("At least one number was < 20: \tTrue\n");
-
-  
-
-else printf("At least one number was < 20: \tFalse\n");
-
-if (tween = 1)
-
-  
-
-printf("All numbers were (10 <= n <= 90): \tTrue\n");
-
-  
-
-else printf("All numbers were (10 <= n <= 90): \tFalse\n");
-
- 
-
-
-return 0;
-
-}
